f2d4865726386db8c6f0807ee9e0b6d6e886d445
DenisLazarian <denis.lazarian.emanuel@gmail.com>
2023-07-21 13:41:19 +0200

    feat(api-classes): Adding a jwt authentication filter

---
+++ src/main/java/com/web/app/flourishandblotts/config/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,76 @@
+package com.web.app.flourishandblotts.config.filters;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.web.app.flourishandblotts.config.jwt.JwtUtils;
+import com.web.app.flourishandblotts.models.UserEntity;
+import jakarta.annotation.Resource;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    @Resource
+    private JwtUtils jwtUtils;
+
+//    public JwtAuthenticationFilter(JwtUtils jwtUtils){
+//        this.jwtUtils=jwtUtils;
+//    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        String username;
+        String password;
+
+        try {
+            UserEntity userEntity = new ObjectMapper().readValue(request.getInputStream(), UserEntity.class);
+            username = userEntity.getMail();
+            password = userEntity.getPassword();
+
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(username, password);
+
+        return getAuthenticationManager().authenticate(authenticationToken);
+    }
+
+
+    /**
+     * If the authentication is successful, here will generate the token.
+     *  */
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        User user  = (User) authResult.getPrincipal();
+        String token = this.jwtUtils.generateAccessToken(user.getUsername());
+
+        response.addHeader("Authorization", token);
+
+        Map<String, Object> httpResponse = new HashMap<>();
+        httpResponse.put("token", token);
+        httpResponse.put("Message", "Authenticated");
+        httpResponse.put("Username", user.getUsername());
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(httpResponse));
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().flush();
+
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+}
