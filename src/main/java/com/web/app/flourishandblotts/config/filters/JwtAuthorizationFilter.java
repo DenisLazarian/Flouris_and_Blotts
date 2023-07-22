998d04e638b657638ed688750a362ea36d0465b5
DenisLazarian <denis.lazarian.emanuel@gmail.com>
2023-07-22 09:47:39 +0200

    feat(filters): Adding a authorization filter
    We add a filter to filter the actions by roles.

---
+++ src/main/java/com/web/app/flourishandblotts/config/filters/JwtAuthorizationFilter.java
@@ -0,0 +1,50 @@
+package com.web.app.flourishandblotts.config.filters;
+
+import com.web.app.flourishandblotts.config.jwt.JwtUtils;
+import com.web.app.flourishandblotts.services.UserDetailsServiceImpl;
+import jakarta.annotation.Resource;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    @Resource
+    private JwtUtils jwtUtils;
+
+    @Resource
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        String tokenHeader = request.getHeader("Authorization");
+
+        if(tokenHeader != null && tokenHeader.startsWith("Bearer ")){
+            String token = tokenHeader.split("\\s")[1];
+
+            if(jwtUtils.isTokenValid(token)){
+                String username = jwtUtils.getUsernameFromToken(token); // mail and username is the same in the project.
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(username, null, userDetails.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
